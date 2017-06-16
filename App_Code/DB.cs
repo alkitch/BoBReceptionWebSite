7f3f7044e907d79c44dd1f01ac3a8d4a366c2318
alkitch <alan_kitching@hotmail.com>
2017-06-16 17:46:44 +0100

    Battle of Britain Reception Ticket Site
    A generic reception web site- using WebMatrix

---
+++ App_Code/DB.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class BofBOrder
+{
+    public int BofBId { get; set; }
+    public string BofBSurname {get; set;}
+    public string BofBFirstName {get; set;}
+    public string BofBSalutation {get; set;}
+}
